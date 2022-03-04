4b6dc6674dcb07dca0b149a1c4099ac7083aabec
Erick Adriano Nunes da Silva <61701606+ErickNunes97@users.noreply.github.com>
2022-03-04 20:59:57 -0300

    Add files via upload

---
+++ Par_ou_Impar/Par_ou_Impar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Par_ou_Impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nDigite um número para validar se é par ou ímpar: \n");
+            int a = int.Parse(Console.ReadLine());
+
+            if(0 == a % 2)
+            {
+                Console.WriteLine( a + " É um número Par");
+            }
+            
+           else
+            {
+                Console.WriteLine(a + " É um número IMPAR");
+            }
+        }
+    }
+}
